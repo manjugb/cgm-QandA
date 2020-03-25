afdf50bb179fb4a471120e3ed05732192fcc56f7
Manjunath Golla Bala <manjugbmsc@gmail.com>
2020-03-25 15:48:36 +0530

    Added New Methods

--- src/main/java/com/cgm/qanda/util/ValidationUtil.java
+++ src/main/java/com/cgm/qanda/util/ValidationUtil.java
@@ -1,50 +1,94 @@
 package com.cgm.qanda.util;
 
-/**  
-* ValidationUtil.java - This class defines the validation of input as a String,Validation of input and format
-* @author  Manjunath Golla Bala
-* @version 1.0 
-* 
-*/ 
-
-
+/**
+ * ValidationUtil.java - This class defines the validation of input as a
+ * String,Validation of input and format
+ * 
+ * @author Manjunath Golla Bala
+ * @version 1.0
+ * 
+ */
 
 public class ValidationUtil {
-	 
-/**
-* Extracts the user's question from the input arguments.
-*
-* Precondition: 'input' should contain at least one element, the question.
-*
-* @param  input           the command-line arguments.
-* @return                 boolean as false if 'input' contains and more than 256
-* @return                 boolean as true  if 'input' contains between 0 and 255
-*/	
 
-    public static boolean validateLength(String input) {
-        if (input == null || input.length() > 256) {
+	/**
+	 * Extracts the user's question from the input arguments.
+	 *
+	 * Precondition: 'input' should contain at least one element, the question.
+	 *
+	 * @param input the command-line arguments.
+	 * @return boolean as false if 'input' not contains and more than 256 and boolean as true if 'input' contains between 0 and 25
+	 */
+
+	public static boolean validateLength(String input) {
+		if (input == null || input.length() > 256) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Extracts the user's question from the input arguments.
+	 *
+	 * Precondition: 'input' should contain at least one element, the question.
+	 *
+	 * @param input the command-line arguments.
+	 * @return boolean as false if not 'input' contains "\"" and boolean as true if  'input' contains "\""
+	 */
+	public static boolean validateAnswerFormat(String input) {
+		if (input.contains("\"")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Extracts the user's question from the input arguments.
+	 *
+	 * Precondition: 'input' should contain at least one element, the question.
+	 *
+	 * @param input the command-line arguments.
+	 * @return boolean as false if not 'input' contains regular expression("^[0-9]+[a-zA-Z][a-zA-Z0-9]*$") and  boolean as true if  'input' contains regular expression
+	 *         ("^[0-9]+[a-zA-Z][a-zA-Z0-9]*$")
+	 */
+	public static boolean validateIsAlpanumeric(String input) {
+		if (input.contains("^[0-9]+[a-zA-Z][a-zA-Z0-9]*$")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Extracts the user's question from the input arguments.
+	 *
+	 * Precondition: 'input' should contain at least one element, the question.
+	 *
+	 * @param input the command-line arguments.
+	 * @return boolean as false if not 'input' contains regular expression (("^[a-zA-Z0-9]+$")) and  boolean as true if  'input' contains regular expression
+	 *         (("^[a-zA-Z0-9]+$"))
+	 */
+
+	public static boolean validateIsNonAlpanumeric(String input) {
+		if (input.contains("^[a-zA-Z0-9]+$")) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Extracts the user's question from the input arguments.
+	 *
+	 * Precondition: 'input' should contain at least one element, the question.
+	 *
+	 * @param input the command-line arguments 
+	 * @return boolean as true if not 'input' contains is not null or not empty string and boolean as true if input is null or empty
+	 */
+
+	public static boolean validateEmpty(String input) {
+		if(input != null && !input.isEmpty())
             return false;
-        }
         return true;
-    }
-    
-/**
- * validateAnswerFormat - This method validate the format 
- * @return                 boolean as false if not 'input' contains "\""
- * @return                 boolean as true if not 'input' contains "\""
- */
-    public static boolean validateAnswerFormat(String input) {
-        if (input.contains("\"")) {
-            return true;
-        }
-        return false;
-    }
-    
-  public static boolean  validateIsAlpanumeric(String input) {
-	  if (input.contains("^[0-9]+[a-zA-Z][a-zA-Z0-9]*$")) {
-		  return true;
-	  }
-	return false;
-  }
-    
+	}
+
+
 }
