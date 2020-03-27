7135b24641d3b0e35400446d04fa7d18a921a6fe
Manjunath Golla Bala <manjugbmsc@gmail.com>
2020-03-27 10:09:41 +0530

    And and modified methods

--- src/main/java/com/cgm/qanda/util/ValidationUtil.java
+++ src/main/java/com/cgm/qanda/util/ValidationUtil.java
@@ -22,7 +22,7 @@ public class ValidationUtil {
 	 */
 
 	public static boolean validateLength(String input) {
-		if (input == null || input.length() > 256 || input == "") {
+		if (input == null || input.length() > 256) {
 			return false;
 		}
 		return true;
@@ -53,29 +53,13 @@ public class ValidationUtil {
 	 *         ("[A-Za-z0-9]+")
 	 */
 	public static boolean validateIsAlpanumeric(String input) {
-		if (input.matches("[A-Za-z0-9]+?") || input.contains("?")) {
+		if (input.matches("[A-Za-z]+?") || input.contains("?")) {
 			return true;
 		}else
 		return false;
 	}
 
-	/**
-	 * Extracts the user's question from the input arguments.
-	 *
-	 * Precondition: 'input' should contain at least one element, the question.
-	 *
-	 * @param input the command-line arguments.
-	 * @return boolean as false if not 'input' contains regular expression (("^[a-zA-Z0-9]+$")) and  boolean as true if  'input' contains regular expression
-	 *         (("[!@#$%&*()_+=|<>?{}\\[\\]~-]"))
-	 */
-
-	public static boolean validateIsSpecialchar(String input) {
-		if (input.contains("^[!@#$%&*()_+=|<>?{}\\\\[\\\\]~-]")) {
-			return true;
-		}
-		return false;
-		
-	}
+	
 	
 	/**
 	 * Extracts the user's question from the input arguments.
@@ -86,11 +70,39 @@ public class ValidationUtil {
 	 * @return boolean as true if not 'input' contains is not null or not empty string and boolean as true if input is null or empty
 	 */
 
-	public static boolean validateEmpty(String input) {
+	public static  boolean validateEmpty(String input) {
 		if(input != null && !input.isEmpty())
             return false;
         return true;
 	}
+	//this is the sample test method to check ParameterisedTest
+	public int add(int a,int b) {
+		int c = a + b;
+		return c;
+		
+	}
+	
+		
+	
+	/**
+	 * Extracts the user's question from the input arguments.
+	 *
+	 * Precondition: 'input' should contain at least one element, the question.
+	 *
+	 * @param input the command-line arguments.
+	 * @return boolean as false if 'input' not contains and more than 256 and boolean as true if 'input' contains between 0 and 255
+	 */
 
+	public static boolean validateLengthRegex(String input) {
+		if (input == null || input.length() > 256) {
+			return false;
+		}else if (input.matches("^.{0,255}$")) {
+			
+			return true;
+		}
+		return false;
+		
+	}
+	
 
 }
